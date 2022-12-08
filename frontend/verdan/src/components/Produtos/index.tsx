ac6c0760db2222f1732097cb3c4aced642482063
AlanDiogoR <alandiogor@gmail.com>
2022-12-07 21:41:26 -0300

    produto faz oque vc tem que fazer e é isso to

--- frontend/verdan/src/components/Produtos/index.tsx
+++ frontend/verdan/src/components/Produtos/index.tsx
@@ -1,20 +1,10 @@
+import styles from './styles.module.scss'
 import boneca from '../../assets/images/b1.jpg';
+
 
 export function Produtos() {
   return (
-    <div>
-      
-      <div>
-        <img src={boneca} alt="Protuto" />
-      </div>
-
-      <div className="informacaoProduto">
-        <label>Boneca</label>
-      </div>
-
-      <div className="precoProduto">
-        <a href="#jjj">R$70.80</a>
-      </div>
-    </div>
+    <>fazer card de produto bem bonito</>
+    
   );
 }
