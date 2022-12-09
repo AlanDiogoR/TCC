b2750c8b0a68e49e1e5ab788470dff5ee8768579
AlanDiogoR <alandiogor@gmail.com>
2022-12-09 14:48:33 -0300

    footer daquele jeito

--- frontend/verdan/src/components/Ofertas/index.tsx
+++ frontend/verdan/src/components/Ofertas/index.tsx
@@ -1,5 +1,21 @@
+import styles from "./styles.module.scss";
+import boneca from '../../assets/images/b1.jpg';
+import { Produtos } from "../Produtos";
+
 export function Ofertas() {
   return (
-    <>s</>
+    <main className={styles.ofertasDaSemana}>
+      <h2>Ofertas da semana</h2>
+      <div>
+      <div className={styles.icon}>&lt;</div> {/*sinal de menor */}
+      <div className={styles.produtos}>
+        <Produtos/>
+        <Produtos/>
+        <Produtos/>
+      </div>
+      <div className={styles.icon}>&gt;</div>{/*sinal de maior */}
+      
+      </div>
+    </main>
   )
 }
