ba7ed2ade1a608da68de5ac039ea3f353444a145
AlanDiogoR <95175918+AlanDiogoR@users.noreply.github.com>
2023-03-14 14:41:48 -0300

    14/03/23
    init

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
-const express = require('express'); 
-
-const app = express();
-
-const port = 3333;
-
-app.listen(`${port}`);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.listen(3001, () => {
+    console.log('🚀 Server is running on https://localhost:3001');
+});
---
+++ backend/src/index.ts
@@ -0,0 +1,7 @@
+import express  from 'express';
+
+const app = express();
+
+app.listen(3001, () => {
+  console.log('🚀 Server is running on https://localhost:3001');
+});
