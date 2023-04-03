93d27e3c95b40132a2d357e8b5052e2ac9afd5ce
AlanDiogoR <95175918+AlanDiogoR@users.noreply.github.com>
2023-04-03 09:21:35 -0300

    03/04/2023

--- frontend/verdanzin/src/pages/index.tsx
+++ frontend/verdanzin/src/pages/index.tsx
@@ -1,12 +1,16 @@
+import { NavCategories, NavSearch } from '@/styles/pages/home';
+
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <h1>oi</h1>
-=======
-    <div>
-      <h1>Home</h1>
-    </div>
->>>>>>> 15a67009b012d29834ddb1fe6234b051316413b0
+    <>
+      <NavSearch>
+        Verdan
+      </NavSearch>
+
+      <NavCategories>
+        Boneca
+      </NavCategories>
+    </>
   );
 }
 
--- frontend/verdanzin/src/styles/pages/home.ts
+++ frontend/verdanzin/src/styles/pages/home.ts
@@ -1,68 +1,9 @@
 import { styled } from '..';
 
-export const HomeContainer = styled('main', {
-  display: 'flex',
-  //gap: '3rem',
-  width: '100%',
-  maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
-  marginLeft: 'auto',
-  minHeight: 656,
+export const NavSearch = styled('div', {
+
 });
 
-export const Product = styled('a', {
-  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
-  borderRadius: 8,
-  //padding: '0.25rem',
-  cursor: 'pointer',
-  position: 'relative',
-  overflow: 'hidden',
-  minWidth: 540,
-
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-
-  img: {
-    objectFit: 'cover',
-  },
-
-  footer: {
-    position: 'absolute',
-    bottom: '0.25rem',
-    left: '0.25rem',
-    right: '0.25rem',
-    padding: '2rem',
-
-    borderRadius: 8,
-
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-
-    backgroundColor: 'rgba(0, 0, 0, 0.6)',
-
-    transform: 'translateY(110%)',
-    opacity: 0,
-    transition: 'all 0.2s ease-in-out',
-
-    strong: {
-      fontSize: '$lg',
-      color: '$gray100',
-    },
-
-    span: {
-      fontSize: '$xl',
-      fontWeight: 'bold',
-      color: '$green300',
-    }
-
-  },
-
-  '&:hover': {
-    footer: {
-      transform: 'translateY(0%)',
-      opacity: 1,
-    }
-  }
+export const NavCategories = styled('nav', {
 
 });
