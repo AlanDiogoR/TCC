9a3c4c1502dd02dec2ce3f6fda43b24c85d2a752
AlanDiogoR <95175918+AlanDiogoR@users.noreply.github.com>
2023-05-04 15:13:40 -0300

    04/05/23
    Header 1.0

--- frontend/verdanzin/src/pages/index.tsx
+++ frontend/verdanzin/src/pages/index.tsx
@@ -1,17 +1,28 @@
 import LOGO  from '@/assets/logo/verdan_logo.png';
-import { NavCategories, NavSearch } from '@/styles/pages/home';
+import { NavCategories, NavIcons, NavSearch, Research } from '@/styles/pages/home';
+import { FaShoppingBag, FaHeart } from 'react-icons/fa';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <NavSearch>
+
         <Image
           src={LOGO}
-          width={520}
-          height={480}
+          width={160}
+          height={80}
           alt=''
         />
+
+        <Research>
+          pesquise aqui
+        </Research>
+        
+        <NavIcons>
+          <FaHeart/>
+        </NavIcons>
+
       </NavSearch>
 
       <NavCategories>
--- frontend/verdanzin/src/styles/pages/home.ts
+++ frontend/verdanzin/src/styles/pages/home.ts
@@ -1,9 +1,21 @@
 import { styled } from '..';
 
 export const NavSearch = styled('div', {
-
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  padding: '1.8rem'
 });
 
 export const NavCategories = styled('nav', {
+
+});
+
+export const Research = styled('div', {
+
+});
+
+export const NavIcons = styled('div', {
 
 });
