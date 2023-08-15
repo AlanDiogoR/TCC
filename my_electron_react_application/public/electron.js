7955ef5e11b83359204a6604ee9fd8b2cbbdaa56
AlanDiogoR <95175918+AlanDiogoR@users.noreply.github.com>
2023-08-15 15:26:11 -0300

    iniciado o,login admin

--- my_electron_react_application/public/electron.js
+++ my_electron_react_application/public/electron.js
@@ -9,8 +9,8 @@ let mainWindow;
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: '100%',
+    height: '100%',
     webPreferences: { nodeIntegration: true, contextIsolation: false },
   });
   // and load the index.html of the app.
