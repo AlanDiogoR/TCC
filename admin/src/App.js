a2c8f47fc71cd7ccc87637fc423607d7bbaa5239
AlanDiogoR <alandiogor@gmail.com>
2023-10-31 01:27:59 -0300

    iniciando admin

---
+++ admin/src/App.js
@@ -0,0 +1,11 @@
+import { LoginAdmin } from './login';
+import './App.css';
+import { RegisterProduct } from './registerProduct';
+
+export default function App() {
+  return (
+    <div>
+      <RegisterProduct/>
+    </div>
+  );
+}
